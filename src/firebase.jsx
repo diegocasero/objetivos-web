39b5de9b5e88db328a2e7126b5bdf4d3284d5165
diegocasero <diego.casero@alumnos.upm.es>
2025-07-10 03:02:48 +0200

    usando variables de entorno

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -2,20 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCOzYN1fmrTrAmseH9lUxtpj2foThWLdnw",
-  authDomain: "objetivos-779ed.firebaseapp.com",
-  projectId: "objetivos-779ed",
-  storageBucket: "objetivos-779ed.firebasestorage.app",
-  messagingSenderId: "1056454066401",
-  appId: "1:1056454066401:web:b934154a45443b033177a0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
